4fcf45acc482f094541aab353ad4e6300c80bf1a
evilset2 <evilset3@gmail.com>
2021-09-17 21:02:39 -0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,85 @@
+var delayInMilliseconds = 5000
+
+setTimeout(function() {
+	  plant1 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(1) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant1 = (plant1[1]/plant1[2]).toFixed(2) + " LE/h"
+	  
+	  console.log(text_plant1)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(1) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant1
+	  
+	  plant2 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(2) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant2 = (plant2[1]/plant2[2]).toFixed(2) + " LE/h"
+	  
+	  console.log(text_plant2)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(2) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant2
+	  
+	  plant3 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(3) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant3 = (plant3[1]/plant3[2]).toFixed(2) + " LE/h"
+	  
+	  console.log(text_plant3)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(3) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant3
+	  
+	  plant4 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(4) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant4 = (plant4[1]/plant4[2]).toFixed(2) + " LE/h"
+	  
+	  console.log(text_plant4)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(4) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant4
+	  
+	  plant5 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(5) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant5 = (plant5[1]/plant5[2]).toFixed(2) + " LE/h"
+	  
+	  console.log(text_plant5)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(5) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant5
+	  
+	  plant6 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(6) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant6 = (plant6[1]/plant6[2]).toFixed(2) + " LE/h"
+	  
+	  console.log(text_plant6)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(6) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant6
+	  
+	  plant7 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(7) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant7 = (plant7[1]/plant7[2]).toFixed(2) + " LE/h"
+	  
+	  console.log(text_plant7)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(7) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant7
+	  
+	  plant8 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(8) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant8 = (plant8[1]/plant8[2]).toFixed(2) + " LE/h"
+	  
+	  console.log(text_plant8)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(8) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant8
+	  //Plant 9
+	  plant9 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(9) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant9 = (plant9[1]/plant9[2]).toFixed(2) + " LE/h"
+	  
+	  console.log(text_plant9)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(9) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant9
+	  //Plant 10
+	  
+      plant10 = (document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(10) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText.replace("\/",' ').split(" "));
+	  
+	  text_plant10 = (plant10[1]/plant10[2]).toFixed(2) + " LE/h"
+	
+	  console.log(text_plant10)
+	  
+	  document.querySelector("#__layout > div > div:nth-child(1) > div.marketplace.tw-flex.tw-flex-row.tw-space-x-4 > div.tw-flex-grow.tw-p-6.center > div > div.tw-p-1.scroll > ul > li:nth-child(10) > a > div.tw-flex.tw-justify-between.tw-items-end > div:nth-child(1) > div").innerText = text_plant10
+	  
+}, delayInMilliseconds);
